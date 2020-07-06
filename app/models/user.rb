f240945546d96507700a51e6c8daf0d102fe8cd9
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-05 21:35:11 -0400

    added has many to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+    has_many :worlds
+    has_many :characters, through :worlds
+    has_many :items, through :characters
+
+    validates_presence_of :name
+    has_secure_password
+
+
+    def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+            user.provider = auth.provider
+            user.uid = auth.uid
+            user.name = auth.info.name
+            user.password = SecureRandom.hex
+          end
+        end
+
 end
