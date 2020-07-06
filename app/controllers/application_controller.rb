36cc4124303af1672a5fd473bcda791406023fbe
wonderfulkasey <kaseyarnold15@gmail.com>
2020-07-05 21:19:59 -0400

    added controller and changed html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        session[:name]
-      end
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def require_login
+    redirect_to login_path,
+      {alert: 'You must be logged in access that page'} unless
+      session.include? :user_id
+  end
 end
